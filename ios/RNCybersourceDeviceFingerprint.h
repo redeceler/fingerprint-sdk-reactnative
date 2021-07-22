d030ee3a6e176238b279f51c9abd6a66bd467fd8
Jhonatan F. Ciriaco <jhonatan.ciriaco.maitha@trademaster.com.br>
2021-07-22 17:56:42 -0300

    firt commit

---
+++ ios/RNCybersourceDeviceFingerprint.h
@@ -0,0 +1,17 @@
+//
+//  RNCybersourceDeviceFingerprint.h
+//
+//  Created by Jhonatan F. Ciriaco on 09/07/21.
+//  Copyright © 2018. All rights reserved.
+//
+
+#if __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
+#import <React/RCTBridgeModule.h>
+#endif
+
+
+@interface RNCybersourceDeviceFingerprint : NSObject <RCTBridgeModule>
+
+@end
